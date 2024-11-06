ceaadc9a030ad0debf8503dcc54b74807617ea1e
Karolina <Karolina@MacBook-Air-7.local>
2024-11-06 14:49:53 +0000

    Changed whole code into function

--- Pandas_RD_TD.py
+++ Pandas_RD_TD.py
@@ -6,24 +6,29 @@ import certifi
 # Set the default SSL context using certifi's certificate bundle
 ssl._create_default_https_context = ssl.create_default_context  
 
-#Load the RD test directory file from website
-url = "https://www.england.nhs.uk/wp-content/uploads/2018/08/national-genomic-test-directory-rare-and-inherited-disease-eligibility-criteria-v7-updated.xlsx"
-df = pd.read_excel(url, sheet_name='R&ID indications', skiprows=[0])
+# Define function
+def load_and_filter_TD ():
 
-# Define the list of values to filter in column E
-desired_values = [
-    "Medium panel",
-    "Small panel",
-    "Small panel - deep sequencing",
-    "WES",
-    "WES or Large Panel",
-    "WES or Medium panel",
-    "WES or Small Panel",
-    "WGS"
-]
+    #Load the RD test directory file from website
+    url = "https://www.england.nhs.uk/wp-content/uploads/2018/08/national-genomic-test-directory-rare-and-inherited-disease-eligibility-criteria-v7-updated.xlsx"
+    df = pd.read_excel(url, sheet_name='R&ID indications', skiprows=[0])
 
-# Filter rows where column E contains any of the desired values
-filtered_df = df[df['Test Method'].isin(desired_values)]
+    # Define the list of values to filter in column E
+    desired_values = [
+        "Medium panel",
+        "Small panel",
+        "Small panel - deep sequencing",
+        "WES",
+        "WES or Large Panel",
+        "WES or Medium panel",
+        "WES or Small Panel",
+        "WGS"
+    ]
 
-# Display the result
-print(filtered_df)
+    # Filter rows where column E contains any of the desired values
+    filtered_df = df[df['Test Method'].isin(desired_values)]
+
+    return filtered_df
+
+# Give back filtered data
+print (load_and_filter_TD())
