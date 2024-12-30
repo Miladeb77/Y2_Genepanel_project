242e79e0362a34b6ba53d964c080f62df0b66f34
nourmahfel <104623680+nourmahfel@users.noreply.github.com>
2024-12-30 10:35:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     packages=find_packages(include=["PanelGeneMapper", "PanelGeneMapper.*"]),  # Correctly include the package and its submodules
     include_package_data=True,  # Includes non-code files like config files
     description="A tool for integrating PanelApp data with lab systems and generating reports.",
-    author="Nour Mahfel",
     python_requires=">=3.9",  # Enforces minimum Python version
 )
